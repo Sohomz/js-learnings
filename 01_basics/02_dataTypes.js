4293f54efcb7613dcb26ae9cf8762ed53867c09c
Sohomz <39939163+Sohomz@users.noreply.github.com>
2024-05-01 16:35:07 +0000

    dataTypes commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // only can be run at browser level
+
+let name="sohom"
+let age=24
+let isLogin=false
+let state=null
+
+// number => 2 to the power 53
+// bigInt
+// string =>""
+// boolean => true/false
+// null => standalone value let state=null
+// undefined =>
+// symbol => unique (used in figma, react etc)
+//object
+
+console.log(typeof(null)) //object
+console.log(typeof(undefined)) //undefined
+
